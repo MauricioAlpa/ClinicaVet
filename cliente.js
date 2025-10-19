2ab66465492f0f04381d4a2837ade9f77287bef0
Guilherme Loiola Bruni Fernandes Florence <guiflorence05@gmail.com>
2025-10-19 14:54:31 -0300

    class adm added

--- cliente.js
+++ cliente.js
@@ -41,20 +41,44 @@ class Cliente {
 
 class Adm {
     listadeclientes = []
-    lsitademedicos = []
+    listademedicos = []
     registrarCliente(nome, especie, raça) {
         let p_saude = false 
         for (const ClienteExisteente of this.listadeclientes) {
             if (ClienteExisteente.getNome() === nome && ClienteExisteente.getEspecie() === especie && ClienteExisteente.getRaça() === raça) {
-                p_saude = true
-                break
+                ClienteExisteente.setP_saude(true)
+                console.log(`Cliente '${nome}' já existe. Plano de saúde atualizado para: true.`)
+                return
             }
         }
         const novoCliente = new Cliente(nome, especie, raça, p_saude)
 
     this.listadeclientes.push(novoCliente)
 
-    console.log(`Cliente '${nome}' registrado com sucesso. Planod de saúde: ${p_saude}.`)
+    console.log(`Cliente '${nome}' registrado com sucesso. Plano de saúde: ${p_saude}.`)
     }
-
+    listarclientes() {
+        console.log("Lista de Clientes:")
+        this.listadeclientes.forEach((cliente, index) => {
+            console.log(`${index + 1}. Nome: ${cliente.getNome()}, Espécie: ${cliente.getEspecie()}, Raça: ${cliente.getRaça()}, Plano de Saúde: ${cliente.getP_saude()}`)
+        })
+    }
 }
+
+const minhaClinica = new Adm();
+
+
+console.log("--- Registrando Clientes ---");
+minhaClinica.registrarCliente("Bolinha", "Cachorro", "Poodle");
+minhaClinica.registrarCliente("Frajola", "Gato", "Siamês");
+minhaClinica.registrarCliente("Rex", "Cachorro", "Pastor Alemão");
+
+
+minhaClinica.listarclientes();
+
+
+console.log("\n--- Registrando um cliente repetido ---");
+minhaClinica.registrarCliente("Bolinha", "Cachorro", "Poodle");
+
+// 5. Vamos listar DE NOVO e ver a mágica do p_saude
+minhaClinica.listarclientes();
